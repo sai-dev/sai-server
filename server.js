d7e89b7b8cc5cafa21f178a771af2db73d12a52d
Gianluca Amato <gianluca.amato@unich.it>
2018-06-29 14:04:56 +0200

    Fix evaluation function for positions.

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ var SELFPLAY_EXPIRE_TIME = 30 * 60 * 1000; // selfplays expire after 30 minutes.
 
 function analyze_sgf_comments (comment) {
     [alpkt, beta, pi, avg_eval, avg_bonus] = comment.split(",").map(parseFloat);
-    return -alpkt;
+    return 0.25-avg_eval*(1-avg_eval);
 }
 
 function analyze_sgf(sgf) {
