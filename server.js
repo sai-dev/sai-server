8afbe963eb4ab42dfe4f377967a2b0e867eece2b
Gianluca Amato <gianluca.amato@unich.it>
2018-08-16 13:25:29 +0200

    Add support for "other_options" parameter in matches and self-plays.

--- server.js
+++ server.js
@@ -36,6 +36,8 @@ const default_no_resignation_probability = configsai.default_no_resignation_prob
 const default_komi = configsai.default_komi ? Number(configsai.default_komi) : 7.5;
 const default_noise_value = configsai.default_noise_value ?  Number(configsai.default_noise_value) : 0.03;
 const default_lambda = configsai.default_lambda ? Number(configsai.default_lambda) : 0.5;
+const default_other_options_selfplay = configsai.default_other_options_selfplay ? String(configsai.default_other_options_selfplay): "";
+const default_other_options_match = configsai.default_other_options_match ? String(configsai.default_other_options_match): "";
 const base_port = configsai.base_port ? Number(configsai.base_port) :  8080;
 const instance_number = configsai.instance_number ? Number(configsai.instance_number) : 0;
 const schedule_matches_to_all = configsai.schedule_matches_to_all ? Boolean(configsai.schedule_matches_to_all) : false;
@@ -189,9 +191,9 @@ function generate_branches(sgf) {
 
 function get_options_hash(options) {
     if (options.visits) {
-        return checksum("" + options.visits + options.resignation_percent + options.noise + options.randomcnt + options.komi + options.noise_value + options.lambda).slice(0, 6);
+        return checksum("" + options.visits + options.resignation_percent + options.noise + options.randomcnt + options.komi + options.noise_value + options.lambda + options.other_options).slice(0, 6);
     } else {
-        return checksum("" + options.playouts + options.resignation_percent + options.noise + options.randomcnt + options.komi + options.noise_value + options.lambda).slice(0, 6);
+        return checksum("" + options.playouts + options.resignation_percent + options.noise + options.randomcnt + options.komi + options.noise_value + options.lambda + options.other_options).slice(0, 6);
     }
 }
 
@@ -619,11 +621,15 @@ app.post('/request-match', (req, res) => {
     if (!req.body.lambda)
         req.body.lambda = default_lambda;
 
+    if (! ('other_options' in req.body))
+        req.body.other_options = default_other_options_match;
+
     const options = { resignation_percent: Number(req.body.resignation_percent),
         randomcnt: Number(req.body.randomcnt),
         noise: String(req.body.noise),
         komi: Number(req.body.komi),
-        lambda: Number(req.body.lambda) };
+        lambda: Number(req.body.lambda),
+        other_options: String(req.body.other_options)};
 
     if (req.body.playouts) {
         options.playouts = Number(req.body.playouts);
@@ -1141,6 +1147,7 @@ app.post("/submit", (req, res) => {
                                             visits: selfplay ? selfplay.visits : default_visits,
                                             resignation_percent: selfplay ? selfplay.resignation_percent : default_resignation_percent,
                                             no_resignation_probability: selfplay ? selfplay.no_resignation_probability : default_no_resignation_probability,
+                                            other_options: selfplay ? selfplay.other_options : default_other_options_selfplay,
                                             number_to_play: 1,
                                             game_count: 0,
                                             networkhash: networkhash,
@@ -1729,7 +1736,7 @@ app.get("/get-task/:autogtp(\\d+)(?:/:leelaz([.\\d]+)?)", asyncMiddleware(async(
 
         //var options = {"playouts": "1600", "resignation_percent": "10", "noise": "true", "randomcnt": "30"};
         const options = { playouts: "0", visits: String(default_visits+1), resignation_percent: String(default_resignation_percent), noise: "true", randomcnt: String(default_randomcnt),
-                          komi: String(default_komi), noise_value: String(default_noise_value), lambda: String(default_lambda) };
+                          komi: String(default_komi), noise_value: String(default_noise_value), lambda: String(default_lambda), other_options: String(default_other_options_selfplay) };
 
         const self_play = shouldScheduleSelfplay(req, now);
         if (self_play) {
@@ -1738,6 +1745,7 @@ app.get("/get-task/:autogtp(\\d+)(?:/:leelaz([.\\d]+)?)", asyncMiddleware(async(
             options.lambda = String(self_play.lambda);
             options.visits = String(self_play.visits);
             options.resignation_percent = String(self_play.resignation_percent);
+            options.other_options = String(self_play.other_options);
             task.hash = String(self_play.networkhash);
             task.selfplay_id = String(self_play._id);
             if (self_play.sgfhash) {
@@ -1791,6 +1799,7 @@ app.post('/request-selfplay',  asyncMiddleware( async (req, res, next) => {
         noise_value: req.body.noise_value  ? parseFloat(req.body.noise_value) : default_noise_value,
         komi: req.body.komi ? parseFloat(req.body.komi) : default_komi,
         lambda: req.body.lambda ? parseFloat(req.body.lambda) : default_lambda,
+        other_options: 'other_options' in req.body ? String(req.body.other_options) : default_other_options_selfplay,
         number_to_play: req.body.number_to_play ? parseInt(req.body.number_to_play) : 1,
         visits: req.body.visits ? parseInt(req.body.visits) : default_visits,
         resignation_percent: req.body.resignation_percent ? parseFloat(req.body.resignation_percent) : default_resignation_percent,
