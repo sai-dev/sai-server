99f3ff2233fde39c00bfb09ddefadb902d640502
Gianluca Amato <gianluca.amato@unich.it>
2019-12-24 19:23:20 +0100

    Change range of x and y axis in the elograph.

--- server.js
+++ server.js
@@ -1718,10 +1718,9 @@ app.get("/", asyncMiddleware(async(req, res) => {
         page += leaderboard_recent_table;
 
         page += "<h4>Recent Strength Graph (<a href=\"/static/newelo.html\">Full view</a>.)</h4>";
-        page += "Currently the graph is manually updated soon after a new network starts self-plays.<br>";
         page += "The plot shows a proper Bayes-Elo rating, computed on the set of all played matches.<br>";
         page += "<h4>The x-axis scale is 1/5 for Leela Zero networks (grey crosses).</h4><br>";
-        page += "<iframe width=\"1100\" height=\"655\" seamless frameborder=\"0\" scrolling=\"no\" src=\"/static/newelo.html?0#recent=2500000\"></iframe><script>(i => i.contentWindow.location = i.src)(document.querySelector(\"iframe\"))</script>";
+        page += "<iframe width=\"1100\" height=\"655\" seamless frameborder=\"0\" scrolling=\"no\" src=\"/static/newelo.html?0#recent=650000\"></iframe><script>(i => i.contentWindow.location = i.src)(document.querySelector(\"iframe\"))</script>";
         page += "<br><br>Times are in GMT+0100 (CET)<br>\n";
         page += network_table;
         page += match_table;
